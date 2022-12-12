b9877c7741cf2629ea52e2836a9245d99463a29c
romkery <romkery@gmail.com>
2022-12-12 17:46:01 +0700

    created base-logo

---
+++ src/utils/BaseLogo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="base-logo">
+    <IconComponent name="LogoIcon" />
+    <span>antools.</span>
+  </div>
+</template>
+
+<script setup>
+import IconComponent from "@/utils/IconComponent";
+</script>
+
+<style lang="scss" scoped>
+@use "../assets/scss/util" as *;
+@use "../assets/scss/globals" as *;
+
+.base-logo {
+  display: flex;
+  align-items: center;
+  color: $white-tp-9;
+  font-size: rem(22);
+  font-weight: 600;
+  gap: rem(12);
+
+  svg {
+    cursor: pointer;
+  }
+}
+</style>
